472a8d4beae7e062bd5191d9a22c65329f4b7d1c
Hongsik <h9yoon@ucsd.edu>
2025-12-23 11:59:52 -0800

    Finalize Valid (Defined Loss function)

---
+++ old_sh/no_prog_noiseless.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# [설명]
+# --training: 학습 모드 켜기
+# --channel_type noiseless: 노이즈 없는 깨끗한 채널 사용
+# --alpha_mode base: 점진적 학습 끄기 (한 번에 통으로 학습)
+
+python main.py \
+    --training \
+    --trainset DIV2K \
+    --testset Kodak \
+    --channel_type noiseless \
+    --alpha_mode base \
+    --train_snr 10
---
+++ old_sh/prog_exp_noiseless.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# [설명]
+# --training: 학습 모드 켜기
+# --channel_type noiseless: 노이즈 없는 깨끗한 채널 사용
+# --alpha_mode base: 점진적 학습 끄기 (한 번에 통으로 학습)
+
+python main.py \
+    --training \
+    --trainset DIV2K \
+    --testset Kodak \
+    --channel_type noiseless \
+    --alpha_mode exponential \
+    --progressive_mode alm \
+    --packet_size   32\
+    --train_snr 10
